1fbc4895cd7d9cbbb2ce80d108b6baaccfbc5f6a
mbazhlekova <maria.bazhlekova@gmail.com>
2017-06-20 21:19:34 -0500

    added route to shorten url and redirect

--- routes/index.js
+++ routes/index.js
@@ -21,21 +21,62 @@ router.get('/new/:url(*)', (req, res, next) => {
       console.log('Unable to connect to server', err);
     } else {
       console.log('Connected to server');
+
       var collection = db.collection('links');
       var params = req.params.url;
-      var newLink = (db, callback) => {};
+
+      var newLink = (db, callback) => {
+        if (validUrl.isUri(params)) {
+          var shortAddr = shortid.generate();
+          var newUrl = {
+            url: params,
+            short: shortAddr
+          };
+          collection.insert([newUrl]);
+          res.json(newUrl);
+        } else {
+          res.json({
+            error: "Wrong URL format, please enter a valid URL."
+          })
+
+        };
+      };
       newLink(db, () => {
         db.close();
       });
-      var insertLink = {
-        url: params,
-        short: 'test'
-      };
-      collection.insert([insertLink]);
-      res.send(params);
-
-    }
+    };
   });
+});
+
+router.get('/:short', (req, res, next) => {
+
+  MongoClient.connect(mongoUrl, (err, db) => {
+    if (err) {
+      console.log("Unable to connect to server", err);
+    }
+    else {
+      console.log("Connected to server");
+      var collection = db.collection('links');
+      var params = req.params.short;
+
+      var findLink = (db, cb) => {
+        collection.findOne({"short": params}, {url: 1, _id: 0}, (err, doc) => {
+          if (doc != null) {
+            res.redirect(doc.url);
+          }
+          else {
+            res.json({error: "Not found in database"})
+          }
+        });
+
+      };
+      findLink(db, () => {
+        db.close();
+      });
+    };
+
+  });
+
 });
 
 module.exports = router;
